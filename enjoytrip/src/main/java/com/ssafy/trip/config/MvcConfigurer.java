ae47e54b5af8dc82a96274eaab6b7849c90a6cbf
손근영 <lllllls189@gmail.com>
2025-05-02 17:39:59 +0900

    test 완료

--- enjoytrip/src/main/java/com/ssafy/trip/config/MvcConfigurer.java
+++ enjoytrip/src/main/java/com/ssafy/trip/config/MvcConfigurer.java
@@ -1,6 +1,7 @@
 package com.ssafy.trip.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,4 +12,12 @@ public class MvcConfigurer implements WebMvcConfigurer {
 	        // "/" 요청이 들어오면 attractionTest.jsp 로 포워딩
 	        registry.addViewController("/").setViewName("forward:/WEB-INF/views/attractionTest.jsp");
 	    }
+	 
+	    @Override
+	    public void addCorsMappings(CorsRegistry registry) {
+	        registry.addMapping("/api/v1/**")
+	                .allowedOrigins("*") // 모든 도메인 허용 (개발용)
+	                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD");
+	    }
+
 }
