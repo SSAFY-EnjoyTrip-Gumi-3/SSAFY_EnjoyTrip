7b561a58caf84540ac2f8bf33d6cfe205b075737
윤준석 <ujs9901@naver.com>
2025-05-09 17:54:17 +0900

    Feat: Swagger에 등록하기 위한 어노테이션 작성

--- enjoytrip/src/main/java/com/ssafy/trip/controller/LikeController.java
+++ enjoytrip/src/main/java/com/ssafy/trip/controller/LikeController.java
@@ -15,20 +15,30 @@ import com.ssafy.trip.model.dto.LikeRequestDTO;
 import com.ssafy.trip.model.dto.LikeResponseDTO;
 import com.ssafy.trip.service.LikeService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/likes")
+@RequestMapping("/api/v1/likes")
+@Tag(name = "likes" , description = "사용자가 좋아요/ 좋아요 취소 조회 API")
 public class LikeController implements RestControllerHelper{
 	private final LikeService lService;
 	
+	@Operation(summary = "좋아요/ 좋아요 취소 결과 조회",
+			 description = "좋아요/ 좋아요 취소한 관광지 정보를 반환합니다.")
+	@ApiResponse(responseCode = "200", description = "성공적으로 좋아요/ 좋아요 취소 결과 반환")
 	@PostMapping
 	public ResponseEntity<LikeResponseDTO> toggleLike(@RequestBody LikeRequestDTO req){
 		LikeResponseDTO resp = lService.toggleLike(req);
 		return ResponseEntity.ok(resp);
 	}
 	
+	@Operation(summary = "현재 좋아요 개수",
+			 description = "관광지의 현재 좋아요 개수를 반환합니다.")
+	@ApiResponse(responseCode = "200", description = "성공적으로 좋아요 개수 결과 반환")
 	@GetMapping("/{attractionNo}")
 	public ResponseEntity<Map<String, Long>> getLikeCount(@PathVariable int attractionNo){
 		long count = lService.getLikeCount(attractionNo);
