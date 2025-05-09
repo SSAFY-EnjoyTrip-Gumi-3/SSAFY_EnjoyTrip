462b19d2c5fa0cc217f0e869e46ee190ec752fdf
윤준석 <ujs9901@naver.com>
2025-05-09 17:29:59 +0900

    Feat: 좋아요 dao 구현

---
+++ enjoytrip/src/main/java/com/ssafy/trip/model/dao/LikeDao.java
@@ -0,0 +1,42 @@
+package com.ssafy.trip.model.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface LikeDao {
+	
+	/**
+     * 특정 관광지(attractionNo)에 대한 총 좋아요(Like) 수를 반환합니다.
+     * 
+     * @param attractionNo 좋아요 수를 계산할 관광지의 식별자
+     * @return 해당 attractionNo에 기록된 좋아요 행의 개수 (long)
+     */
+	long countLikesByAttraction(int attractionNo);
+	
+	/**
+	* 사용자가 특정 관광지에 좋아요를 추가(INSERT)합니다.
+	* 
+	* @param userNo       좋아요를 누른 사용자의 식별자
+	* @param attractionNo 좋아요 대상 관광지의 식별자
+	* @return 삽입된 행(row)의 수 (성공 시 1)
+	*/
+	int insertLike(int userNo, int attractionNo);
+	
+	/**
+     * 사용자가 특정 관광지에 눌렀던 좋아요를 취소(DELETE)합니다.
+     * 
+     * @param userNo       좋아요 취소할 사용자의 식별자
+     * @param attractionNo 좋아요 취소 대상 관광지의 식별자
+     * @return 삭제된 행(row)의 수 (성공 시 1)
+     */
+	int deleteLike(int userNo, int attractionNo);
+	
+	/**
+     * 사용자가 이미 해당 관광지에 좋아요를 눌렀는지 확인합니다.
+     * 
+     * @param userNo       확인할 사용자의 식별자
+     * @param attractionNo 확인할 관광지의 식별자
+     * @return 존재하는 좋아요 레코드 수 (0이면 없음, >0이면 이미 좋아요 상태)
+     */
+	int existsLike(int userNo, int attractionNo);
+}
